4da23da4654540316260a6ed82b975016ce83d2e
Nataly Romanenko <nataliia.romanenko@gmail.com>
2019-05-08 17:54:50 +0300

    start shop

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import {Route, Router} from "react-router";
+import layuot from "./node_modules/containers/layuot/layout";
+import phones from "./node_modules/containers/phones/phones";
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+     <div>
+        <Route component={layuot}>
+            <Route path='/' component={phones}/>
+        </Route>
+     </div>
   );
 }
 
