60724a48559652f2452369eb9c1942a317c4e1bd
Nataly Romanenko <nataliia.romanenko@gmail.com>
2019-05-17 16:00:30 +0300

    btn LoadMore

--- src/App.js
+++ src/App.js
@@ -1,19 +1,22 @@
-import React from 'react';
-import {Route, Router} from "react-router";
+import React, {Component} from 'react';
+import {Route} from "react-router";
 import Layout from "./node_modules/containers/layuot/layout";
 import Phones from "./node_modules/containers/phones/phones";
 
 
 
 
-function App() {
-  return (
-     <div>
-         <Route component={Layout}>
-             <Route path='/' component={Phones} />
-         </Route>
-     </div>
-  );
+class App extends Component {
+    render() {
+        return (
+            <div className="wrapp">
+                <Route component={Layout}>
+                    <Route path='/' component={Phones}/>
+                </Route>
+            </div>
+        );
+    }
 }
+
 
 export default App;
